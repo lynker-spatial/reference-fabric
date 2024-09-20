e449e365cb36fb8dd2fb346914360a3a19498bf7
program-- <justin@justinsingh.me>
2024-09-20 08:27:17 -0700

    feat: extract functions

---
+++ R/utils-targets.R
@@ -0,0 +1,143 @@
+rf.helper.get_conus_vpus <- function() {
+  nhdplusTools::vpu_boundaries |>
+    dplyr::arrange(VPUID) |>
+    dplyr::filter(VPUID <= "18") |>
+    sf::st_make_valid()
+}
+
+#' @export
+rf.targets.get_topology <- function() {
+  boundaries <- rf.helper.get_conus_vpus()
+
+  sf::st_intersects(boundaries, sparse = FALSE) |>
+    which(arr.ind = TRUE) |>
+    apply(MARGIN = 1, \(x) sort(unname(x)), simplify = FALSE) |>
+    unique() |>
+    do.call(what = rbind, args = _) |>
+    as.data.frame() |>
+    stats::setNames(c("VPU1", "VPU2")) |>
+    dplyr::filter(VPU1 != VPU2) |>
+    dplyr::mutate(
+      VPU1 = boundaries$VPUID[VPU1],
+      VPU2 = boundaries$VPUID[VPU2]
+    )
+}
+
+#' @export
+rf.targets.get_enhd <- function(outfile) {
+  sb_id <- "63cb311ed34e06fef14f40a3"
+  sb_name <- "enhd_nhdplusatts.parquet"
+
+  sbtools::item_file_download(
+    sb_id,
+    names = sb_name,
+    destinations = outfile,
+    overwrite_file = TRUE
+  )
+
+  outfile
+}
+
+#' @export
+rf.targets.get_vaa <- function(outfile) {
+  if (!file.exists(outfile)) {
+    tmp <- file.path(tempdir(TRUE), "vaa.parquet")
+    on.exit(unlink(tmp))
+
+    nhdplusTools::get_vaa(
+      path = tmp,
+      download = TRUE,
+      updated_network = FALSE
+    ) |>
+      arrow::write_parquet(outfile)
+  }
+
+  outfile
+}
+
+#' @export
+rf.targets.list_nhd <- function(dir.epa, dir.nhd) {
+  epa_bucket <- "dmap-data-commons-ow"
+  boundaries <- sf::st_drop_geometry(rf.helper.get_conus_vpus())
+
+  urls <- 
+    aws.s3::get_bucket_df(epa_bucket, prefix = "NHDPlusV21/Data/NHDPlus") |>
+    dplyr::select(key = Key) |>
+    dplyr::filter(endsWith(key, ".7z")) |>
+    dplyr::mutate(file = basename(key)) |>
+    dplyr::mutate(
+      split = lapply(strsplit(file, "_", fixed = TRUE), utils::head, -1),
+      region = vapply(split, dplyr::nth, character(1), 2),
+      vpu = vapply(split, dplyr::nth, character(1), 3),
+      type = vapply(split, dplyr::last, character(1))
+    ) |>
+    dplyr::select(key, region, vpu, type) |>
+    dplyr::arrange(vpu, type) |>
+    dplyr::filter(vpu %in% rf.helper.get_conus_vpus()$VPUID) |>
+    dplyr::filter(type %in% c("NHDPlusCatchment", "NHDSnapshot", "NHDPlusBurnComponents")) |>
+    dplyr::mutate(url = paste0("https://", epa_bucket, ".s3.amazonaws.com/", key)) |>
+    dplyr::select(-key)
+  
+  # Format into type-specific data.frame
+  .component <- function(.type, .shp, .fgb) {
+    dplyr::filter(urls, type == !!.type) |>
+      dplyr::mutate(
+        archive = file.path(
+          dir.epa,
+          paste0(type, vpu, ".7z")
+        ),
+        shp = file.path(
+          dir.epa,
+          paste0("NHDPlus", region),
+          paste0("NHDPlus", vpu),
+          !!.type,
+          !!.shp
+        ),
+        fgb = file.path(dir.nhd, !!.fgb, paste0("NHDPlus", vpu, ".fgb"))
+      )
+  }
+
+  dplyr::bind_rows(
+    .component("NHDPlusCatchment", "Catchment.shp", "catchments"),
+    .component("NHDSnapshot", "Hydrography/NHDFlowline.shp", "flowlines"),
+    .component("NHDSnapshot", "Hydrography/NHDWaterbody.shp", "waterbodies"),
+    .component("NHDPlusBurnComponents", "BurnAddLine.shp", "burnlines")
+  )
+}
+
+#' @export
+rf.targets.download_nhd <- function(.row) {
+  .urls    <- dplyr::select(.row, url, destfile = archive)
+  url      <- .urls$url
+  destfile <- .urls$destfile
+
+  if (!file.exists(.row$shp)) {
+    utils::download.file(url, destfile, mode = "wb")
+    archive::archive_extract(destfile, dir = dirname(destfile))
+    unlink(destfile)
+  }
+
+  .row$shp
+}
+
+#' @export
+rf.targets.convert_nhd <- function(shp, .tbl) {
+  fgb <- dplyr::filter(.tbl, shp == !!shp)$fgb
+
+  if (length(fgb) == 0) {
+    stop("Invalid shapefile path: ", shp)
+  }
+
+  sf::gdal_utils(
+    "vectortranslate",
+    shp,
+    fgb,
+    options = c(
+      "-f", "FlatGeobuf",
+      "-makevalid",
+      "-nlt", "PROMOTE_TO_MULTI"
+    )
+  )
+
+  fgb
+}
